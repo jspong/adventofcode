a7f1bcda4ee66c5299c9f1c1b10039bb432a69ee
John Spong <jspong@gmail.com>
2019-12-04 13:27:15 -0500

    Day 02 done

--- 2019/02.py
+++ 2019/02.py
@@ -15,25 +15,35 @@ def groups(iterator, size):
 def main(argv):
     with open(argv[1], 'r') as handle:
         inputs = list(opstream(handle))
-    pos = 0
+    target = int(argv[2])
 
     # Run gravity assist program 1202
-    inputs[1] = 12
-    inputs[2] = 2
+    for verb in range(100):
+        for noun in range(100):
+            copied_input = list(inputs)
+            copied_input[1:3] = [noun, verb]
+            a = answer(copied_input)
+            if a == target:
+                print("%02d%02d" % (noun, verb))
+                return
+    else:
+        print("ERROR: no noun/verb found")
 
+def answer(inputs):
+    pos = 0
     while True:
         opcode, a, b, c = inputs[pos:pos+4]
-        print("%s: %s, %s, %s, %s" % (','.join(str(i) for i in inputs), opcode, a, b, c))
         if opcode == 1:
             inputs[c] = inputs[a] + inputs[b]
+            pos += 4
         elif opcode == 2:
             inputs[c] = inputs[a] * inputs[b]
+            pos += 4
         elif opcode == 99:
             break
         else:
-            raise Exception("illegal opcode %d at position %d: %s", opcode, pos, ','.join(inputs))
-        pos += 4
-    print(inputs[0])
+            raise Exception("illegal opcode %s at position %s: %s" % (opcode, pos, ','.join(str(i) for i in inputs)))
+    return inputs[0]
 
 
 if __name__ == '__main__':
